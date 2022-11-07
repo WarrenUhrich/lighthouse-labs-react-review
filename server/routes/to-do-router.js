40f40d8ca49a8a20066b3a73304f91b0727111b8
Warren <hello@warren.codes>
2022-11-07 09:29:32 -0700

    Add create to-do and complete to-do server features

--- server/routes/to-do-router.js
+++ server/routes/to-do-router.js
@@ -1,5 +1,6 @@
 const toDos = require('../db/to-dos');
 const express = require('express');
+const uniqid = require('uniqid');
 const router = express.Router();
 
 //////////////////////////////////////////////////////////////////
@@ -8,6 +9,41 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json(toDos);
+});
+
+//////////////////////////////////////////////////////////////////
+// Create
+//////////////////////////////////////////////////////////////////
+
+router.post('/', (req, res) => {
+    const newTitle = req.body.title;
+    const newTask = req.body.task;
+
+    console.log('form data received:', req.body);
+
+    const newToDo = {
+        id: uniqid(),
+        title: newTitle,
+        task: newTask,
+        completed: false,
+    };
+
+    toDos[newToDo.id] = newToDo;
+
+    res.json(newToDo);
+});
+
+//////////////////////////////////////////////////////////////////
+// Complete To-Do
+//////////////////////////////////////////////////////////////////
+
+router.patch('/:id/complete', (req, res) => {
+    if (toDos[req.params.id]) {
+        toDos[req.params.id].completed = true;
+        res.json(toDos[req.params.id]);
+    } else {
+        res.json({error: 'ID not found.'})
+    }
 });
 
 module.exports = router;
